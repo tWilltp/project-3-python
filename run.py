dfd7841c802125eec3cbd80835b20a84cf60d41d
tWilltp <williamjenkinsfeb@gmail.com>
2022-12-07 16:14:43 +0000

    add basic def structure

---
+++ run.py
@@ -0,0 +1,33 @@
+def see_instructions():
+    pass
+
+
+def player_info():
+    pass
+
+
+def player_board():
+    pass
+
+
+def enemy_board():
+    pass
+
+
+def place_player_ships():
+    pass
+
+
+def generate_enemy_ships():
+    pass
+
+
+def enemy_strategy():
+    pass
+
+def hit_miss():
+    pass
+
+
+def end_of_game():
+    pass
