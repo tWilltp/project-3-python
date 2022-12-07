bb7c142b828755a689602d42927e7e8761e33f9d
tWilltp <williamjenkinsfeb@gmail.com>
2022-12-07 16:23:37 +0000

    add coordinate conversions (letters to numbers etc) add more def's to count shots fired and ships sunk

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
+coordinate_conversions_letters = {
+    'A': 0,
+    'B': 0,
+    'C': 0,
+    'D': 0,
+    'E': 0,
+    'F': 0,
+    'G': 0,
+    'H': 0
+}
+
+coordinate_conversions_numbers = {
+    '1': 0,
+    '2': 0,
+    '3': 0,
+    '4': 0,
+    '5': 0,
+    '6': 0,
+    '7': 0,
+    '8': 0
+}
+
+
 def see_instructions():
     pass
 
@@ -25,7 +48,16 @@ def generate_enemy_ships():
 def enemy_strategy():
     pass
 
+
 def hit_miss():
+    pass
+
+
+def count_ships_hits():
+    pass
+
+
+def count_shots():
     pass
 
 
