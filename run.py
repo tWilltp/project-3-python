789176e660e977107c50aa117ab8fbd13e8f6ec6
tWilltp <williamjenkinsfeb@gmail.com>
2022-12-12 13:57:16 +0000

    created input attack coordinates function for player

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#Legend
+# Legend
 # X for placing ship and hit ship
 # ' ' for available space
 # '-' for missed shot
@@ -9,6 +9,7 @@ from random import randint
 
 ENEMY_BOARD = [[' '] * 8 for x in range(8)]
 PLAYER_BOARD = [[' '] * 8 for x in range(8)]
+board = [[' '] * 8 for x in range(8)]
 
 coordinate_conversions_letters = {
     'A': 0,
@@ -42,14 +43,24 @@ def display_board(board):
         row_number += 1
 
 
-def generate_enemy_ships():
+def generate_enemy_ships(board):
     for ship in range(5):
         ship_row, ship_column = randint(0, 7), randint(0, 7)
-        while board[ship_row] 
+        while board[ship_row][ship_column] == 'X':
+            ship_row, ship_column = randint(0, 7), randint(0, 7)
+        board[ship_row][ship_column] = 'X'
 
 
 def attack_coordinates():
-    pass
+    row = input('Please enter a ship row 1 - 8')
+    while row not in '12345678':
+        print('Please enter a valid row')
+        row = input('Please enter a ship row 1 - 8')
+    column = input('Please enter a ship column A - H').upper()
+    while column not in 'ABCDEFGH':
+        print('Please enter a valid column')
+        column = input('Please enter a ship column A - H').upper()
+    return int(row) - 1, coordinate_conversions_letters[column]
 
 
 # def see_instructions():
@@ -81,3 +92,7 @@ def count_shots():
 def end_of_game():
     pass
 
+
+display_board(board)
+generate_enemy_ships(board)
+attack_coordinates()
