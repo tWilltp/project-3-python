d0d7ac586bad2b8925d04d33556fc7e5611d3458
tWilltp <williamjenkinsfeb@gmail.com>
2023-01-20 16:23:19 +0000

    changed shots_fired function so that missed hits are registered as '-' player cannot hit own ships and enemy ships hit are marked with 'S' to represent sunk battleship

--- run.py
+++ run.py
@@ -6,13 +6,13 @@
 
 from random import randint
 
-game_board = [[' '] * 9 for x in range(10)]
+game_board = [[' '] * 9 for x in range(9)]
 enemy_ships = []
 player_ships = []
 enemy_strikes = []
 player_strikes = []
-row = int in range(9)
-column = str in range(20)
+row = int in range(8)
+column = str in range(8)
 
 convert_let_to_num = {
     'A': 0,
@@ -24,7 +24,6 @@ convert_let_to_num = {
     'G': 6,
     'H': 7,
     'I': 8,
-    'J': 9,
 }
 
 convert_num_to_num = {
@@ -37,7 +36,6 @@ convert_num_to_num = {
     '7': 6,
     '8': 7,
     '9': 8,
-    '0': 9,
 }
 
 
@@ -49,12 +47,14 @@ def run_game():
     generate_enemy_ships(game_board)
     display_board(game_board)
     attack_coordinates()
+    shots_fired(game_board)
+    count_ships_hit()
     #  check_player_strikes(row, column)
 
 
 def display_board(game_board):
     print('Your board')
-    print('  A B C D E F G H I J')
+    print('  A B C D E F G H I')
     row_number = 1
     for row in game_board:
         print("%d|%s|" % (row_number, "|".join(row)))
@@ -63,40 +63,50 @@ def display_board(game_board):
 
 def generate_player_ships(game_board):
     for ship in range(5):
-        ship_row, ship_column = randint(0, 9), randint(0, 99)
+        ship_row, ship_column = randint(0, 8), randint(0, 8)
         while game_board[ship_row][ship_column] == 'X':
-            ship_row, ship_column = randint(0, 9), randint(0, 9)
+            ship_row, ship_column = randint(0, 8), randint(0, 8)
         game_board[ship_row][ship_column] = 'X'
 
 
 def generate_enemy_ships(game_board):
     for ship in range(5):
-        ship_row, ship_column = randint(0, 9), randint(0, 9)
+        ship_row, ship_column = randint(0, 8), randint(0, 8)
         while game_board[ship_row][ship_column] == '0':
-            ship_row, ship_column = randint(0, ), randint(0, 9)
-        game_board[ship_row][ship_column] = ' '
+            ship_row, ship_column = randint(0, 8), randint(0, 8)
+        game_board[ship_row][ship_column] = '0'
 
 
 def attack_coordinates():
-    row = input('Please enter a ship row 1 - 0: ')
+
+    # Enter the row number between 1 to 8
+
+    row = input('Please enter a ship row 1 - 9: ')
     while row not in '123456789':
         print('Please enter a valid row')
-        row = input('Please enter a ship row 1 - 0: ')
-    column = input('Please enter a ship column A - J: ').upper()
+        row = input('Please enter a ship row 1 - 9: ')
+
+    # Enter the Ship column from A TO H
+
+    column = input('Please enter a ship column A - I: ').upper()
     while column not in 'ABCDEFGHIJKLMNOPQRST':
         print('Please enter a valid column')
-        column = input('Please enter a ship column A - J: ').upper()
-    return int(row), convert_let_to_num[column]
+        column = input('Please enter a ship column A - I: ').upper()
+    return int(row)-1, convert_let_to_num[column]
 
 
 def shots_fired(game_board):
     turns = 25
-    while turns <= 25:
+    while turns > 0:
+        display_board(game_board)
+        row, column = attack_coordinates()
         if game_board[row][column] == '-':
-            print('Area Clear')
+            print('Area clear, guess again')
         elif game_board[row][column] == 'X':
+            print('Friendly fire, guess again')
+        elif game_board[row][column] == '0':
             print('Target Hit!')
-            game_board[row][column] = 'X'
+            game_board[row][column] = 'S'
             turns -= 1
         else:
             print('Shot missed')
@@ -115,7 +125,7 @@ def count_ships_hit():
     count = 0
     for row in game_board:
         for column in row:
-            if column == 'X':
+            if column == '0':
                 count += 1
     return count
 
