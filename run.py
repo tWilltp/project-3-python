2702f72e531657f5a4f43a85e1c045e2070acc22
tWilltp <williamjenkinsfeb@gmail.com>
2023-01-23 12:19:37 +0000

    add computer shot def, bug; computer fires until all player ships hit, then fires turn for turn

--- run.py
+++ run.py
@@ -79,7 +79,7 @@ def generate_enemy_ships(game_board):
 
 def attack_coordinates():
 
-    # Enter the row number between 1 to 8
+    # Enter the row number between 1 to 9
 
     row = input('Please enter a ship row 1 - 9: ')
     while row not in '123456789':
@@ -95,27 +95,70 @@ def attack_coordinates():
     return int(row)-1, convert_let_to_num[column]
 
 
-def shots_fired(game_board):
+def computer_shot(game_board):
+
     turns = 25
     while turns > 0:
+
+        display_board(game_board)
+        row, column = randint(0, 8), randint(0, 8)
+
+        if game_board[row][column] == ' ':
+            print('computer missed')
+            game_board[row][column] = '-'
+            turns -= 1
+
+        elif game_board[row][column] == '0':
+            computer_shot(game_board)
+
+        elif game_board[row][column] == 'X':
+            print('You lost a ship')
+            game_board[row][column] = 'L'
+            turns -= 1
+
+        else:
+            print('Computer shot missed')
+            game_board[row][column] = '-'
+            turns -= 1
+
+        if count_comp_ships_hit() == 5:
+            print("Battle Lost! we'll get 'em next time")
+            break
+
+        print('You have ' + str(count_comp_ships_hit()) + ' ships left')
+
+
+def shots_fired(game_board):
+
+    turns = 25
+    while turns > 0:
+
         display_board(game_board)
         row, column = attack_coordinates()
+
         if game_board[row][column] == '-':
             print('Area clear, guess again')
+
         elif game_board[row][column] == 'X':
             print('Friendly fire, guess again')
+
         elif game_board[row][column] == '0':
             print('Target Hit!')
             game_board[row][column] = 'S'
             turns -= 1
+
         else:
             print('Shot missed')
             game_board[row][column] = '-'
             turns -= 1
+
         if count_ships_hit() == 5:
             print('Enemy defeated, Well done!')
             break
+
+        computer_shot(game_board)
         print('You have ' + str(turns) + ' shots left')
+        
         if turns == 0:
             print("Battle Lost! we'll get 'em next time")
             break
@@ -125,26 +168,18 @@ def count_ships_hit():
     count = 0
     for row in game_board:
         for column in row:
-            if column == '0':
+            if column == 'S':
                 count += 1
     return count
 
 
-def hit_miss():
-    pass
-
-
-def count_shots():
-    pass
-
-
-def end_of_game():
-    pass
+def count_comp_ships_hit():
+    count = 0
+    for row in game_board:
+        for column in row:
+            if column == 'L':
+                count += 1
+    return count
 
 
 run_game()
-# count_ships_hit()
-# shots_fired(game_board)
-
-# def player_info():
-# pass
